dd5ffeac77fd631af92378d65cdb8be1fc83631e
nfebrian13 <nanaz.febrian@gmail.com>
2020-10-04 11:49:12 +0700

    initial commit

---
+++ springboot-cloud-m2-configserver/src/main/java/com/confserver/SpringbootCloudM2ConfigserverApplication.java
@@ -0,0 +1,20 @@
+package com.confserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringbootCloudM2ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudM2ConfigserverApplication.class, args);
+	}
+	
+/*	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
+	public String sayHello() {
+		return "Hello from Spring Boot";
+	} */
+
+}
---
+++ springboot-cloud-m2-configserver/src/test/java/com/confserver/SpringbootCloudM2ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudM2ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
