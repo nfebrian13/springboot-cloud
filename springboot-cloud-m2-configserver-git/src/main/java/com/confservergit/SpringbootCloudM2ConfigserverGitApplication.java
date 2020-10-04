0004485fae8ae3a45427804a0b75c9c4d3af9572
nfebrian13 <nanaz.febrian@gmail.com>
2020-10-04 12:36:53 +0700

    initial commit

---
+++ springboot-cloud-m2-configserver-git/src/main/java/com/confservergit/SpringbootCloudM2ConfigserverGitApplication.java
@@ -0,0 +1,15 @@
+package com.confservergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringbootCloudM2ConfigserverGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudM2ConfigserverGitApplication.class, args);
+	}
+
+}
---
+++ springboot-cloud-m2-configserver-git/src/test/java/com/confservergit/SpringbootCloudM2ConfigserverGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confservergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudM2ConfigserverGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
