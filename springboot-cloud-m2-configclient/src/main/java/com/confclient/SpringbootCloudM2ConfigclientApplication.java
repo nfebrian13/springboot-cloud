7d7ab01328dcd74d7b9c01cc37d4b8c438d516d5
nfebrian13 <nanaz.febrian@gmail.com>
2020-10-04 13:18:20 +0700

    commit

---
+++ springboot-cloud-m2-configclient/src/main/java/com/confclient/SpringbootCloudM2ConfigclientApplication.java
@@ -0,0 +1,13 @@
+package com.confclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCloudM2ConfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudM2ConfigclientApplication.class, args);
+	}
+
+}
---
+++ springboot-cloud-m2-configclient/src/test/java/com/confclient/SpringbootCloudM2ConfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudM2ConfigclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
